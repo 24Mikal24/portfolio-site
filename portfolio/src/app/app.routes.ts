e43bcecc2244620db855a1221d33e3bc4bb693d6
Michael Archer <mikal@Michaels-Laptop.lan>
2024-07-02 03:00:15 +0800

    add initial styling and components

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'resume', component: ResumeComponent },
+    { path: 'contact', component: ContactComponent }
+];
