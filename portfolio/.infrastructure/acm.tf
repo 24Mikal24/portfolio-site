857992dc7ec41b9c1466117e0c75cdef06207a35
Michael Archer <mikal@Michaels-Laptop.lan>
2024-07-15 04:44:19 +0800

    refactor infrastructure to use cloudfront distribution, create ssl certs, and hosted zone

---
+++ portfolio/.infrastructure/acm.tf
@@ -0,0 +1,16 @@
+resource "aws_acm_certificate" "ssl_certificate" {
+  domain_name = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
+  validation_method = "DNS"
+
+  tags = var.common_tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
---
+++ portfolio/.infrastructure/terraform.tfvars
@@ -0,0 +1,6 @@
+domain_name = "mikearcher.dev"
+bucket_name = "mikearcher.dev"
+
+common_tags = {
+  Project = "mikearcher.dev"
+}
