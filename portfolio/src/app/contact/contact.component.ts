56a1de97825bb837cf6a5ece9542bdab3b177dcc
Michael Archer <mikearcherdev@proton.me>
2024-07-24 02:48:52 +0800

    setup initial contact form to work with api gateway endpoint

--- portfolio/src/app/contact/contact.component.ts
+++ portfolio/src/app/contact/contact.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  form: FormGroup;
 
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  sendEmail() {
+    this.http.post('https://0eu6lmq8g2.execute-api.us-east-1.amazonaws.com/default/contactFormLambda', this.form.value).subscribe({
+      next: (res) => this.displaySuccessMessage(),
+      error: (err) => this.displayErrorMessage()
+    });
+  }
+
+  displaySuccessMessage() {
+    this.form.reset();
+    console.log("Successfully sent email");
+  }
+
+  displayErrorMessage() {
+    this.form.reset();
+    console.log("There was an issue sending your message");
+  }
 }
